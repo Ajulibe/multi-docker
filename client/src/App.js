b21f738b97c9abbad7615c907f141c67b5a56b86
akachukwu <akachi_benji@yahoo.com>
2020-06-24 07:54:02 +0100

    chnages

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
